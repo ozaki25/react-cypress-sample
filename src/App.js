0fe5cf570967e47c52c1d81a9f7e179e24e78cb5
ozaki <fs65ugumori@yahoo.co.jp>
2019-09-25 00:56:54 +0900

    update sample

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import Typography from './components/Typography';
 import TextInput from './components/TextInput';
 import Button from './components/Button';
 import Container from './components/Container';
-import { isUniformNumber } from './utils/index';
+import { isUniformNumber } from './utils';
+import { members } from './constants/members';
 
 function App() {
   const [value, setValue] = useState('');
   const [valid, setValid] = useState(true);
+  const [result, setResult] = useState(null);
   const onChange = e => setValue(e.target.value.trim());
   const onClick = () => {
     const isValid = isUniformNumber(value);
     setValid(isValid);
+    setResult(null);
     if (isValid) {
+      console.log({ members });
+      setResult({ name: members[value], number: value });
       setValue('');
-      alert(value);
     }
   };
   return (
@@ -30,6 +34,13 @@ function App() {
           の選手は誰?
         </Typography>
         <Button onClick={onClick}>送信</Button>
+        {result && (
+          <Typography>
+            {`背番号 ${result.number} 番の選手は ${
+              result.name ? `${result.name}です` : 'いません'
+            }`}
+          </Typography>
+        )}
       </Container>
     </div>
   );
