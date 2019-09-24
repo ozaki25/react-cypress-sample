59eb0973f4bf980b753b352abcc55a6070d047fd
ozaki <fs65ugumori@yahoo.co.jp>
2019-09-24 23:42:33 +0900

    add error text to Typography component

--- src/components/Typography/index.js
+++ src/components/Typography/index.js
@@ -5,10 +5,11 @@ const P = styled.p`
   font-size: 1.2em;
   font-weight: 600;
   margin: 0 0 15px;
+  color: ${({ error }) => (error ? '#d32f2f' : '#000')};
 `;
 
-function Typography({ children }) {
-  return <P>{children}</P>;
+function Typography({ children, error }) {
+  return <P error={error ? 1 : 0}>{children}</P>;
 }
 
 export default Typography;
--- src/components/Typography/index.test.js
+++ src/components/Typography/index.test.js
@@ -3,3 +3,8 @@ import { snapshot } from '../../test/helper';
 import Typography from '.';
 
 snapshot('Typography', <Typography>通常の文章です</Typography>);
+
+snapshot(
+  'Typography error text',
+  <Typography error>エラーの文章です</Typography>,
+);
