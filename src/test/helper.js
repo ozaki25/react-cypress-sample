eb3f128a5a5e8db3d43efe8db391ddec9cf01157
ozaki <fs65ugumori@yahoo.co.jp>
2019-09-24 23:02:43 +0900

    apply jest-styled-components

--- src/test/helper.js
+++ src/test/helper.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import 'jest-styled-components';
 
 export const snapshot = (title, element) => {
   it(`should match snapshot: ${title}`, () => {
